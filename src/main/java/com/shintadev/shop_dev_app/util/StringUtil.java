60cf0da6906c07c69156b052c9c45055ea96f2de
shintadev <84170345+shintadev@users.noreply.github.com>
2025-01-16 22:07:21 +0700

    update: add MIT license and update project metadata in pom.xml

--- src/main/java/com/shintadev/shop_dev_app/util/StringUtil.java
+++ src/main/java/com/shintadev/shop_dev_app/util/StringUtil.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 public class StringUtil {
 
   private StringUtil() {
-    // private constructor to hide the implicit public one
   }
 
   public static boolean isVNMobile(String mobile) {
