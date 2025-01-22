429a96faaadae2e05085b3e51a279c99a8ec297f
shintadev <84170345+shintadev@users.noreply.github.com>
2025-01-22 12:03:18 +0700

    update: update OrderController, ProductController, and UserController to use ResponseEntity for responses; implement pagination for getAll methods

--- src/main/java/com/shintadev/shop_dev_app/controller/OrderController.java
+++ src/main/java/com/shintadev/shop_dev_app/controller/OrderController.java
@@ -1,14 +1,19 @@
 package com.shintadev.shop_dev_app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shintadev.shop_dev_app.dto.user.UserCreateDto;
@@ -16,55 +21,51 @@ import com.shintadev.shop_dev_app.model.Order;
 import com.shintadev.shop_dev_app.service.OrderService;
 
 @RestController
+@RequestMapping("/api/orders")
 public class OrderController {
 
   @Autowired
   private OrderService orderService;
 
-  @PostMapping("/add")
-  @ResponseStatus(code = HttpStatus.CREATED)
-  public String add(@RequestBody UserCreateDto user) {
+  @PostMapping
+  public ResponseEntity<?> add(@RequestBody UserCreateDto user) {
     // TODO: process POST request
 
-    return "";
+    return new ResponseEntity<>(user, HttpStatus.CREATED);
   }
 
   @GetMapping("/{id}")
-  @ResponseStatus(code = HttpStatus.OK)
-  public Order getById(@PathVariable String id) {
+  public ResponseEntity<?> getById(@PathVariable String id) {
     // TODO: process GET request
 
-    return new Order();
+    return new ResponseEntity<>(id, HttpStatus.OK);
   }
 
-  // @GetMapping("/all")
-  // @ResponseStatus(code = HttpStatus.OK)
-  // public Page<User> getAll(
-  // @RequestParam int page,
-  // @RequestParam(defaultValue = "10") int size,
-  // @RequestParam(defaultValue = "id") String sort,
-  // @RequestParam(defaultValue = "desc") String order) {
-  // int pageNumber = Math.max(0, page - 1);
-  // Sort.Direction direction = order.equalsIgnoreCase("asc") ? Sort.Direction.ASC
-  // : Sort.Direction.DESC;
-  // Sort sortBy = Sort.by(direction, sort);
-  // Pageable pageable = PageRequest.of(pageNumber, size, sortBy);
-  // return userService.findAll(pageable);
-  // }
+  @GetMapping
+  public ResponseEntity<?> getAll(
+      @RequestParam int page,
+      @RequestParam(defaultValue = "10") int size,
+      @RequestParam(defaultValue = "id") String sort,
+      @RequestParam(defaultValue = "desc") String order) {
+    int pageNumber = Math.max(0, page - 1);
+    Sort.Direction direction = order.equalsIgnoreCase("asc") ? Sort.Direction.ASC
+        : Sort.Direction.DESC;
+    Sort sortBy = Sort.by(direction, sort);
+    Pageable pageable = PageRequest.of(pageNumber, size, sortBy);
+    return new ResponseEntity<>(orderService.findAll(pageable), HttpStatus.OK);
+  }
 
   @PatchMapping("/{id}")
-  @ResponseStatus(code = HttpStatus.OK)
-  public String update(@PathVariable String id, @RequestBody String entity) {
+  public ResponseEntity<?> update(@PathVariable String id, @RequestBody String entity) {
     // TODO: process PATCH request
 
-    return entity;
+    return new ResponseEntity<>(entity, HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
-  @ResponseStatus(code = HttpStatus.NO_CONTENT)
-  public String delete(@PathVariable String id) {
+  public ResponseEntity<?> delete(@PathVariable String id) {
     // TODO: process DELETE request
 
-    return id;
+    return new ResponseEntity<>(id, HttpStatus.NO_CONTENT);
   }
 }
--- src/main/java/com/shintadev/shop_dev_app/controller/UserController.java
+++ src/main/java/com/shintadev/shop_dev_app/controller/UserController.java
@@ -1,52 +1,44 @@
 package com.shintadev.shop_dev_app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shintadev.shop_dev_app.dto.user.UserCreateDto;
-import com.shintadev.shop_dev_app.model.User;
 import com.shintadev.shop_dev_app.service.UserService;
-
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
-@Slf4j
 public class UserController {
 
   @Autowired
   private UserService userService;
 
-  @PostMapping("/add")
-  @ResponseStatus(code = HttpStatus.CREATED)
-  public String add(@RequestBody UserCreateDto user) {
-    return userService.create(user).toString();
+  @PostMapping
+  public ResponseEntity<?> add(@RequestBody UserCreateDto user) {
+    return new ResponseEntity<>(userService.create(user), HttpStatus.CREATED);
   }
 
   @GetMapping("/{id}")
-  @ResponseStatus(code = HttpStatus.OK)
-  public User getById(@PathVariable String id) {
-    return userService.findOne(Long.parseLong(id));
+  public ResponseEntity<?> getById(@PathVariable String id) {
+    return new ResponseEntity<>(userService.findOne(Long.parseLong(id)), HttpStatus.OK);
   }
 
-  @GetMapping("/all")
-  @ResponseStatus(code = HttpStatus.OK)
-  public Page<User> getAll(
+  @GetMapping
+  public ResponseEntity<?> getAll(
       @RequestParam int page,
       @RequestParam(defaultValue = "10") int size,
       @RequestParam(defaultValue = "id") String sort,
@@ -55,20 +47,20 @@ public class UserController {
     Sort.Direction direction = order.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
     Sort sortBy = Sort.by(direction, sort);
     Pageable pageable = PageRequest.of(pageNumber, size, sortBy);
-    return userService.findAll(pageable);
+    return new ResponseEntity<>(userService.findAll(pageable), HttpStatus.OK);
   }
 
   @PatchMapping("/{id}")
-  public String update(@PathVariable String id, @RequestBody String entity) {
+  public ResponseEntity<?> update(@PathVariable String id, @RequestBody String entity) {
     // TODO: process PATCH request
 
-    return entity;
+    return new ResponseEntity<>(entity, HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
-  public String delete(@PathVariable String id) {
+  public ResponseEntity<?> delete(@PathVariable String id) {
     // TODO: process DELETE request
 
-    return id;
+    return new ResponseEntity<>(id, HttpStatus.OK);
   }
 }
