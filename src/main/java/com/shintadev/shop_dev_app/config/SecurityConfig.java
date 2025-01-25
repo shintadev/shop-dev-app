544fe1958443023822be816db54758d4e5b4e853
shintadev <84170345+shintadev@users.noreply.github.com>
2025-01-25 21:23:06 +0700

    feat: add Spring Security configuration with custom password encoder and login/logout handling

---
+++ src/main/java/com/shintadev/shop_dev_app/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.shintadev.shop_dev_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder() {
+      @Override
+      public String encode(CharSequence rawPassword) {
+        return rawPassword.toString();
+      }
+
+      @Override
+      public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return rawPassword.toString().equals(encodedPassword);
+      }
+    };
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.formLogin((formLogin) -> formLogin
+        .loginPage("/login")
+        .loginProcessingUrl("/login")
+        .defaultSuccessUrl("/home", true)
+        .failureUrl("/login?error=true")
+        .permitAll());
+
+    httpSecurity.logout((logout) -> logout
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/login")
+        .permitAll());
+
+    httpSecurity.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+        .requestMatchers("/login").permitAll()
+        .requestMatchers("/register").permitAll()
+        .requestMatchers("/home").authenticated()
+        .requestMatchers("/admin").hasRole("ADMIN")
+        .anyRequest().authenticated());
+
+    return httpSecurity.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails admin = User.builder()
+        .username("admin")
+        .password(passwordEncoder().encode("admin"))
+        .roles("ADMIN", "USER")
+        // .authorities("CAN_READ", "CAN_WRITE")
+        .build();
+
+    UserDetails user = User.builder()
+        .username("user")
+        .password(passwordEncoder().encode("user"))
+        .roles("USER")
+        .build();
+
+    return new InMemoryUserDetailsManager(admin, user);
+  }
+}
